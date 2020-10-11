d0e1d2dc45ebd70a3a881be91870e4eacae8e93c
Merrick919 <37953774+Merrick919@users.noreply.github.com>
2020-10-11 23:18:22 +0800

    Added more comments

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const webhookURL = ("https://discordapp.com/api/webhooks/" + webhookID + "/" + w
 
 const database = "database.txt";
 
+// Function to write data to the database file.
 function writeToFile(content, filename) {
 
     let filepath = (".\\" + filename);
@@ -69,6 +70,7 @@ function writeToFile(content, filename) {
 
 }
 
+// Function to read data from the database file.
 function readFromFile(filename) {
 
     let filepath = (".\\" + filename);
@@ -109,42 +111,49 @@ function readFromFile(filename) {
     
 }
 
+// Function to get the target user's full name.
 function getUserFullName(jsonData) {
 
     return jsonData["graphql"]["user"]["full_name"];
 
 }
 
-function  getTotalPhotos(jsonData) {
+// Function to get the target user's total image/post count.
+function getTotalImages(jsonData) {
 
     return Math.round(jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]);
 
 }
 
+// Function to get the target user's last publication URL.
 function getLastPublicationURL(jsonData) {
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node.shortcode"];
 
 }
 
-function getLastPhotoURL(jsonData) {
+// Function to get the target user's last image/post URL.
+function getLastImageURL(jsonData) {
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"];
 
 }
 
+// Function to get the target user's last thumbnail URL.
 function getLastThumbURL(jsonData) {
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"];
 
 }
-    
-function getDescriptionPhoto(jsonData) {
+
+// Function to get the target user's image/post description.
+function getImageDescription(jsonData) {
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"];
 
 }
 
+// Function to interact with the Discord webhook.
 function webhook(jsonData) {
 
     try {
@@ -158,7 +167,7 @@ function webhook(jsonData) {
         embed["color"] = 15467852;
         embed["title"] = ("New pic of @" + targetInstagramUsername);
         embed["url"] = ("https://www.instagram.com/p/" + getLastPublicationURL(jsonData) + "/");
-        embed["description"] = getDescriptionPhoto(jsonData);
+        embed["description"] = getImageDescription(jsonData);
         // embed["image"] = {"url":get_last_thumb_url(jsonData)}; // Uncomment this to send a bigger image to Discord.
         embed["thumbnail"] = {"url":getLastThumbURL(jsonData)};
         data["embeds"].append(embed);
@@ -183,10 +192,12 @@ function webhook(jsonData) {
 
 }
 
+// The main function to test for new images/posts and interact with the Discord webhook if there is/are (a) new image(s)/post(s).
 async function main() {
      
     try {
 
+        // The function to test for new images/posts.
         function test(jsonData) {
             
             let timeNow = new Date();
@@ -208,6 +219,7 @@ async function main() {
 
         const jsonData = await fetch(targetInstagramURL).then(res => res.json());
 
+        // Wait 20 seconds so there is a bigger total delay between checks for new images/posts (total: 20 seconds) and so the content can be retrieved. 
         setTimeout(function() { test(jsonData); }, 20000);
 
     } catch (err) {
@@ -222,5 +234,5 @@ async function main() {
 
 }
 
-// Start testing for new images every 20 seconds.
+// Start the main function every 20 seconds.
 setInterval(function() { main(); }, 20000);
