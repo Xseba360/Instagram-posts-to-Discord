3940b5833a2eb11e5a853cb71e2d43f5b55468e6
Merrick919 <37953774+Merrick919@users.noreply.github.com>
2020-10-13 23:54:27 +0800

    Fixed all bugs, updated a lot of code

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# IS NOT WORKING CURRENTLY, I'M STILL FIXING IT
-
 # Instagram-posts-to-Discord
 By [Fernando](https://github.com/dlfernando/)\
 Modified and translated from Python to JavaScript by [Merrick919](https://github.com/Merrick919)\
@@ -7,16 +5,16 @@ Original repo: [https://github.com/dlfernando/Instagram-to-discord](https://gith
 
 ### Description
 
-This script monitors an Instagram account and send images to a Discord channel by a webhook when new images/posts are found.
+This script monitors an Instagram account and send images to a Discord channel by a webhook, in an embed, when new posts are found.
 
 It executes 2 actions:
-1. The script checks for a new image posted in an Instagram account every 20 seconds.
-2. If a new image is found, the script posts the new Instagram image to a Discord channel by a webhook.
+1. The script checks for a new post in an Instagram account every 20 seconds.
+2. If a new post is found, the script sends the new Instagram image to a Discord channel by a webhook, in an embed.
 
 ### Requirements
 
 - Node.js
-- dotenv, node-fetch, webhook-discord, fs and chalk modules
+- dotenv, node-fetch, discord.js and chalk modules
 
 ### Usage
 
@@ -27,21 +25,21 @@ You should create a .env file with the variables listed below:
 4. `DELAY`
 5. `DISCORD_EMBED_COLOUR`
 
-.env file example:
+.env file example (this is just an example and will not work):
 ```
 TARGET_INSTAGRAM_USERNAME=xxxxxx
-DISCORD_WEBHOOK_ID=xxxxxx
-DISCORD_WEBHOOK_TOKEN=xxxxxx
+DISCORD_WEBHOOK_ID=0123456789
+DISCORD_WEBHOOK_TOKEN=qwertyASDF012345
 DELAY=20000
-DISCORD_EMBED_COLOUR=7fffd4
+DISCORD_EMBED_COLOUR=5851DB
 ```
 
 For `TARGET_INSTAGRAM_USERNAME`, it's simply the username of the account you want to monitor.\
 For `DISCORD_WEBHOOK_ID` and `DISCORD_WEBHOOK_TOKEN`, first create a webhook in Discord, then copy the webhook URL.\
 In the webhook URL below, "0123456789" is the `DISCORD_WEBHOOK_ID` and "qwertyASDF012345" is the `DISCORD_WEBHOOK_TOKEN`. This is just an example and will not work.\
 https://discordapp.com/api/webhooks/0123456789/qwertyASDF012345
-`DELAY` is the amount of time in milliseconds between checks for new images/posts (the delay before the first check will be tripled).\
-`DISCORD_EMBED_COLOUR` is the hex code of a colour (an integer).
+`DELAY` is the amount of time in milliseconds between checks for new posts. If the delay is too short it might not work. Also note that the delay before the first check will be tripled.\
+`DISCORD_EMBED_COLOUR` is the hex code of a colour (without the hash (#)).
 
 You can run this script just by navigating to the project directory with command prompt then running the command `npm start` or `node main.js`.\
-I recommend using [nodemon](https://www.npmjs.com/package/nodemon). You can run the command `nodemon main.js` if you have it.
+I recommend you use [nodemon](https://www.npmjs.com/package/nodemon). You can run the command `nodemon main.js` if you have it.
--- main.js
+++ main.js
@@ -30,14 +30,17 @@ const fs = require("fs");
 // Requires the chalk module.
 const chalk = require("chalk");
 
+// Create a new webhook client.
+const discordWebhookClient = new Discord.WebhookClient(discordWebhookID, discordWebhookToken);
+
 // The target Instagram URL.
 const targetInstagramURL = ("https://www.instagram.com/" + targetInstagramUsername + "/?__a=1");
 
 // The database file (just a text file).
 const database = "database.txt";
 
-// Create a new webhook client.
-const discordWebhookClient = new Discord.WebhookClient(discordWebhookID, discordWebhookToken);
+// Set the database file path.
+const filepath = (".\\" + database);
 
 // Function to get the current time in ISO format.
 function timeNowISO() {
@@ -49,118 +52,81 @@ function timeNowISO() {
 
 }
 
-// Function to read data from the database file.
-async function readFromFile(filename) {
+// Function to log a message to the console.
+function consoleLog(message) {
 
-    let filepath = (".\\" + filename);
-
-    fs.access(filepath, fs.constants.R_OK, (err) => {
-
-        if (err) {
-
-            console.error(err);
-            console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured trying to read the file \"" + filename + "\"."));
-            
-        } else {
-            
-            fs.readFile(filepath, "utf8", (err, data) => {
-
-                if (err) {
-
-                    console.error(err)
-                    console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured trying to read the file \"" + filename + "\"."));
-
-                } else {
-
-                    return data;
-
-                }
-
-            });
-            
-        }
-
-    });
-    
-}
-
-// Function to write data to the database file.
-async function writeToFile(filename, content) {
-
-    let filepath = (".\\" + filename);
-
-    fs.access(filepath, fs.constants.R_OK, (err) => {
-
-        if (err) {
-
-            console.error(err);
-            console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured trying to read the file \"" + filename + "\"."));
-            
-        } else {
-            
-            fs.writeFile(filepath, content, err => {
-
-                if (err) {
-
-                    console.error(err);
-                    console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured trying to write to the file \"" + filename + "\"."));
-
-                }
-
-            });
-            
-        }
-
-    });
+    console.log(chalk.blue(timeNowISO()) + " " + message);
 
 }
 
+// You can use this to get the target user's full name.
+/*
 // Function to get the target user's full name.
 function getUserFullName(jsonData) {
+
+    consoleLog("Retrieving the target user's full name…");
 
     return jsonData["graphql"]["user"]["full_name"];
 
 }
+*/
 
 // Function to get the target user's avatar URL.
 function getAvatarURL(jsonData) {
+
+    consoleLog("Retrieving the target user's avatar URL…");
 
     return jsonData["graphql"]["user"]["profile_pic_url_hd"];
 
 }
 
-// Function to get the target user's total image/post count.
-function getTotalImages(jsonData) {
+// Function to get the target user's last post URL.
+function getLastPostURL(jsonData) {
 
-    return Math.round(jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]);
-
-}
-
-// Function to get the target user's last publication URL.
-function getLastPublicationURL(jsonData) {
+    consoleLog("Retrieving the target user's last post URL…");
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"];
 
 }
 
-// Function to get the target user's last image/post URL.
+// Function to get the target user's last image URL.
 function getLastImageURL(jsonData) {
+
+    consoleLog("Retrieving the target user's last image URL…");
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"];
 
 }
 
-// Function to get the target user's last thumbnail URL.
+// You can use this to get the target user's last post thumbnail URL.
+/*
+// Function to get the target user's last post thumbnail URL.
 function getLastThumbURL(jsonData) {
+
+    consoleLog("Retrieving the target user's last post thumbnail URL…");
 
     return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"];
 
 }
+*/
 
-// Function to get the target user's image/post description.
-function getImageDescription(jsonData) {
+// Function to get the target user's last post caption.
+// There might not be a caption.
+function getImageCaption(jsonData) {
 
-    return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"];
+    consoleLog("Retrieving the target user's last post caption…");
+
+    try {
+
+        return jsonData["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"];
+
+    } catch (err) {
+
+        consoleLog("No caption for the last post was found.");
+
+        return false;
+
+    }
 
 }
 
@@ -169,16 +135,46 @@ function sendEmbed(jsonData) {
 
     try {
 
+        // For more information about embeds, read the discord.js documentation at https://discord.js.org/#/docs/main/stable/class/MessageEmbed.
+
+        let color = discordEmbedColour;
+        let author = targetInstagramUsername;
+        let authorAvatarURL = getAvatarURL(jsonData);
+        let authorURL = ("https://www.instagram.com/" + targetInstagramUsername + "/");
+        let title = ("New post by @" + targetInstagramUsername);
+        let url = ("https://www.instagram.com/p/" + getLastPostURL(jsonData) + "/");
+        let caption = getImageCaption(jsonData);
+        let image = getLastImageURL(jsonData);
+        let footer = "Instagram-posts-to-Discord";
+        let footerIconURL = "https://i.imgur.com/WhdOxpN.png";
+
         // Create the embed.
-        let embed = new Discord.MessageEmbed()
-            .setColor(discordEmbedColour)
-            .setAuthor(targetInstagramUsername, getAvatarURL(jsonData), ("https://www.instagram.com/" + targetInstagramUsername + "/"))
-            .setTitle("New post by @" + targetInstagramUsername)
-            .setURL("https://www.instagram.com/p/" + getLastPublicationURL(jsonData) + "/")
-            .setDescription(getImageDescription(jsonData))
-            .setImage(getLastThumbURL(jsonData))
-            .setFooter("Instagram-posts-to-Discord")
-            .setTimestamp();
+        if (!caption) {
+
+            // If there is no caption, don't include it.
+            embed = new Discord.MessageEmbed()
+                .setColor(color)
+                .setAuthor(author, authorAvatarURL, authorURL)
+                .setTitle(title)
+                .setURL(url)
+                .setImage(image)
+                .setFooter(footer, footerIconURL)
+                .setTimestamp();
+
+        } else {
+
+            // If there is a caption, include it.
+            embed = new Discord.MessageEmbed()
+                .setColor(color)
+                .setAuthor(author, authorAvatarURL, authorURL)
+                .setTitle(title)
+                .setURL(url)
+                .setDescription(caption)
+                .setImage(image)
+                .setFooter(footer, footerIconURL)
+                .setTimestamp();
+
+        }
 
         // Send the embed.
         discordWebhookClient.send({
@@ -187,16 +183,18 @@ function sendEmbed(jsonData) {
             embeds: [embed],
         });
 
+        consoleLog(chalk.green("Embed sent to Discord."));
+
     } catch (err) {
 
         console.error(err);
-        console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured."));
+        consoleLog(chalk.red("An error occured."));
 
     }
 
 }
 
-// The main function to test for new images/posts and interact with the Discord webhook if there is/are (a) new image(s)/post(s).
+// The main function to test for new images/posts and interact with the Discord webhook if there is/are (a) new post(s).
 async function main() {
      
     try {
@@ -207,26 +205,90 @@ async function main() {
             // This compares the recorded old publication URL in the database file
             // with the newest retrieved publication URL.
 
-            let oldData = await readFromFile(database);
-            let newData = await getLastPublicationURL(jsonData);
+            // Check if file access is okay first.
+            fs.access(filepath, fs.constants.R_OK, (err) => {
+        
+                if (err) {
+        
+                    console.error(err);
+                    consoleLog(chalk.red("An error occured trying to read the file \"" + filename + "\"."));
+                    
+                } else {
+
+                    // Read data from the database file.
+                    fs.readFile(filepath, "utf8", (err, data) => {
+        
+                        if (err) {
+        
+                            console.error(err)
+                            consoleLog(chalk.red("An error occured trying to read the file \"" + filename + "\"."));
+        
+                        } else {
+                            
+                            consoleLog("Data read from " + filepath + ".");
+
+                            oldData = data;
+        
+                        }
+        
+                    });
+                    
+                }
+        
+            });
+
+            let newData = getLastPostURL(jsonData);
 
             async function testData(oldData, newData) {
 
+                consoleLog("Old data: " + oldData);
+                consoleLog("New data: " + newData);
+
                 // If the recorded old publication URL is the same as the newest retrieved publication URL,
-                // it most likely means that there are no new images/posts.
+                // it most likely means that there are no new posts.
                 if (oldData == newData) {
 
-                    console.log(chalk.blue(timeNowISO()) + " " + chalk.yellow("No new image(s)/post(s) found."));
+                    consoleLog(chalk.yellow("No new post(s) found."));
                 
                 // If the recorded old publication URL is not the same as the newest retrieved publication URL,
-                // it most likely means that there is/are (a) new image(s)/post(s).
+                // it most likely means that there is/are (a) new post(s).
                 } else {
                     
                     // Record the new publication URL to the database file.
-                    writeToFile(database, newData);
-                    console.log(chalk.blue(timeNowISO()) + " " + chalk.green("New image(s)/post(s) found."));
+                    consoleLog(chalk.green("New post(s) found."));
+
                     sendEmbed(jsonData);
+
+                    // Check if file access is okay first.
+                    fs.access(filepath, fs.constants.R_OK, (err) => {
+                
+                        if (err) {
+                
+                            console.error(err);
+                            consoleLog(chalk.red("An error occured trying to read the file \"" + filename + "\"."));
                             
+                        } else {
+                            
+                            // Write the new data to the database file.
+                            fs.writeFile(filepath, newData, (err) => {
+                
+                                if (err) {
+                
+                                    console.error(err);
+                                    consoleLog(chalk.red("An error occured trying to write to the file \"" + filename + "\"."));
+                
+                                } else {
+
+                                    consoleLog("New data written to " + filepath + ".");
+
+                                }
+                
+                            });
+                            
+                        }
+                
+                    });
+                    
                 }
 
             }
@@ -245,11 +307,13 @@ async function main() {
     } catch (err) {
 
         console.error(err);
-        console.log(chalk.blue(timeNowISO()) + " " + chalk.red("An error occured."));
+        consoleLog(chalk.red("An error occured."));
 
     }
 
 }
+
+consoleLog("Script started.")
 
 // Start the main function every x milliseconds.
 setInterval(function() { main(); }, delay);
